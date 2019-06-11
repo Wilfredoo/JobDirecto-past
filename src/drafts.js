d3fdf68fdd0cfef1e9ee6b73e2fc00189b71c60a
Wilfredo <casas.farach@gmail.com>
2019-06-11 15:51:40 +0200

    .

--- src/drafts.js
+++ src/drafts.js
@@ -1,13 +1,2 @@
 // this goes in jobs, above the other button
-<Link to="/loginorregister"><input id="createJob" type="submit" value="Busco gente"/></Link>
-<div className="urgentJobs">
-  {this.state.urgentJobData.data.map(data => {
-    return (
-      <div key={data.id}>
-        <p>
-          <span className="restName">{data.restname}</span>
-        </p>
-      </div>
-    );
-  })}
-</div>
+aa;
